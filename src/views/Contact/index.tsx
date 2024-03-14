c8e6e68b9622ed5ccdf73508e361fb3190ff679d
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-14 00:05:21 -0500

    Header and CoverContent components were implemented

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -1,10 +1,314 @@
 import React from "react";
+import CoverContent from "../../components/CoverContent/idex";
+import Header from "../../components/common/Header";
+import ImgContact from "./../../assets/images/contact.avif";
 import "./styles.scss";
 
 const Contact: React.FC = () => {
   return (
     <div className="contact-container">
-      <h1>Contact</h1>
+      <Header />
+
+      <CoverContent bgImg={ImgContact}>
+        <div className="cover-content">Pregúntanos, estamos para ayudarte</div>
+      </CoverContent>
+
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+      </section>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+      </section>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+      </section>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+      </section>
     </div>
   );
 };
--- src/views/Products/index.tsx
+++ src/views/Products/index.tsx
@@ -1,10 +1,92 @@
 import React from "react";
+import CoverContent from "../../components/CoverContent/idex";
+import Header from "../../components/common/Header";
+import ImgProducts from "./../../assets/images/products.avif";
 import "./styles.scss";
 
 const Products: React.FC = () => {
   return (
     <div className="products-container">
-      <h1>Products</h1>
+      <Header />
+
+      <CoverContent bgImg={ImgProducts}>
+        <div className="cover-content">Escoge un estilo vibrante</div>
+      </CoverContent>
+
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
+          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
+          ullam quibusdam blanditiis vel?
+        </p>
+      </section>
     </div>
   );
 };
