bc3d9e1c1261fd7dfa07154ca24823bd66ce29ff
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-13 21:48:41 -0500

    Add parent directives on initial App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Suspense></Suspense>
+    </div>
+  );
 }
 
 export default App;
