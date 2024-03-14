93840d4e0994feb287f618b11fee56dfe14c8326
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-13 22:05:59 -0500

    Initial views components were created

---
+++ src/views/Contact/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Contact: React.FC = () => {
+  return (
+    <div className="contact-container">
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/views/Products/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Products: React.FC = () => {
+  return (
+    <div className="products-container">
+      <h1>Products</h1>
+    </div>
+  );
+};
+
+export default Products;
