57d1ad23f85b138eee84153fa6bf4e3a8f976181
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-16 15:18:21 -0500

    Property to control fixed scroll without effect, was added to Header

--- src/views/Products/components/ProductDetail/index.tsx
+++ src/views/Products/components/ProductDetail/index.tsx
@@ -4,7 +4,7 @@ import Header from "../../../../components/common/Header";
 const ProductDetail: React.FC = () => {
   return (
     <div className="product-detail-container">
-      <Header />
+      <Header hasScrollEffect={false} />
       <h2>Detalles del Producto</h2>
       <p>ID del Producto: {"rggfghgh"}</p>
     </div>
