1a81e5fef8f47fa26a8de1d5fd4dbe44eb252f7e
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-16 13:28:16 -0500

    RedirectionList was implemented on Footer componet

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -3,51 +3,34 @@ import ImgLinkedIn from "../../../assets/icons/linkedin.svg";
 import ImgWhatsApp from "../../../assets/icons/whatsapp.svg";
 import ImgYoutube from "../../../assets/icons/youtube.svg";
 import ImgLogo from "../../../assets/images/logo.png";
+import { footerContent } from "../../../constants";
 import Icon from "../../Icon";
+import RedirectionList from "../../RedirectionList";
 import "./styles.scss";
 
 const Footer = () => {
+  const legalInfo = footerContent.LegalInformation || {};
+  const contactInfo = footerContent.Contact || {};
+
   return (
     <div className="footer-container">
       <div className="footer-redirects">
-        <div className="redirection-list">
-          <span>Información Legal</span>
-          <ul>
-            <li>
-              <a href="##">Términos y Condiciones</a>
-            </li>
-            <li>
-              <a href="##">Política de Devoluciones</a>
-            </li>
-            <li>
-              <a href="##">Política de Privacidad</a>
-            </li>
-          </ul>
-        </div>
+        <RedirectionList
+          title={legalInfo.title}
+          redirects={legalInfo.redirects}
+        />
 
-        <div className="redirection-list">
-          <span>Contacto</span>
-          <ul>
-            <li>
-              <a href="https://co.linkedin.com/in/lfchamorro">Soporte</a>
-            </li>
-            <li>
-              <a href="tel:+57 3118648813">+57 311 864 8813</a>
-            </li>
-            <li>
-              <a href="malito:ing.lfchamorro@gmail.com">
-                ing.lfchamorro@gmail.com
-              </a>
-            </li>
-          </ul>
-        </div>
+        <RedirectionList
+          title={contactInfo.title}
+          redirects={contactInfo.redirects}
+        />
       </div>
       <hr />
 
       <div className="footer-legal">
         <div className="footer-legal-copyright">
           © {new Date().getFullYear()} PagoLC.com Todos los derechos reservados
-          <p>Versión 1.2.1</p>
+          <p>Versión 1.0</p>
         </div>
 
         <div className="footer-legal-logo">
