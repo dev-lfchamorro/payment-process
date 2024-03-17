282ea58c535baa8a97269bfc7321f291fbcc1229
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-17 17:54:32 -0500

    Configuration to save global state for UserInfo was created

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,1 +1,2 @@
+export * from "./paymentActions";
 export * from "./productActions";
--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,11 +1,14 @@
 import { combineReducers } from "redux";
+import paymentReducer, { IPaymentState } from "./paymentReducer";
 import productReducer, { IProductState } from "./productReducer";
 
 export interface IRootState {
+  payments: IPaymentState;
   products: IProductState;
 }
 
 export const rootReducer = combineReducers({
+  payments: paymentReducer,
   products: productReducer,
 });
 
