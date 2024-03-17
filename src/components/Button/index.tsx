93e14c366ea700db4b8a14adcced928614c59654
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-17 11:19:14 -0500

    Button component was created

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Icon from "../Icon";
+import "./styles.scss";
+
+export type ButtonProps = {
+  className?: string;
+  icon: string;
+  text: string;
+  onClick?: () => void;
+};
+
+const Button: React.FC<ButtonProps> = ({ className, icon, text, onClick }) => {
+  return (
+    <div className={`${className}`} onClick={onClick}>
+      <button className="payment-btn">
+        <Icon pathIcon={icon} size={14} brightness={1} />
+        {text}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
