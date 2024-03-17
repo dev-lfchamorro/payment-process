bd3e30fec595194746e97d681d6a92a296c2321b
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-16 23:00:08 -0500

    Navigation and set global state for product selected was implemented when a product was clicked

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,38 +1,57 @@
 import React from "react";
-import { Products } from "../../types";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { paths } from "../../constants";
+import { addProduct } from "../../redux/actions";
+import { Product } from "../../types";
 import "./styles.scss";
 
 type ProductCardProps = {
-  productList: Products[];
+  productList: Product[];
 };
 
 const ProductCard: React.FC<ProductCardProps> = ({ productList }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = (product: Product) => {
+    dispatch(addProduct(product));
+    navigate(`${paths.products}/${product.id}`, { replace: true });
+  };
+
   return (
     <div className="product-cards-container">
-      {productList.map(
-        (
-          { brand, category, image, name, price, discount, hasDiscount },
-          idx
-        ) => (
+      {productList.map((product, idx) => {
+        const {
+          brand,
+          category,
+          image,
+          id,
+          name,
+          price,
+          discount,
+          hasDiscount,
+        } = product;
+
+        return (
           <div
             className="product-card"
             key={idx + 1}
             style={{ backgroundImage: `url(${image})` }}
+            onClick={() => id && handleClick(product)}
           >
             <div className="product-card-category">{category}</div>
             <div className="product-card-detail">
               <span className="product-card-detail-brand">{brand}</span>
               <span className="product-card-detail-name">{name}</span>
-              <span className="product-card-detail-price">
-                {`$COP ${price}`}
-              </span>
+              <span className="product-card-detail-price">{`$COP ${price}`}</span>
             </div>
             {hasDiscount && discount && (
               <span className="product-card-offer">{`${discount}% DTO`}</span>
             )}
           </div>
-        )
-      )}
+        );
+      })}
     </div>
   );
 };
