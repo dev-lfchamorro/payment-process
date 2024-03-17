73a95ac933dec039a8705961a8eec387a8098722
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-17 09:40:41 -0500

    ProductDetail view was completed and refactored

--- src/views/Products/components/ProductDetail/index.tsx
+++ src/views/Products/components/ProductDetail/index.tsx
@@ -1,19 +1,43 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import BlockHeaderSpace from "../../../../components/BlockHeaderSpace";
+import ListItemText from "../../../../components/ListItemText";
 import Footer from "../../../../components/common/Footer";
 import Header from "../../../../components/common/Header";
 import { RootState } from "../../../../redux/reducers";
+import { infoProduct, infoProductValues } from "../../constants";
 import "./styles.scss";
 
 const ProductDetail: React.FC = () => {
   const { selectedProduct } = useSelector((state: RootState) => state.products);
+  const { image, name } = selectedProduct || {};
 
   return (
     <div className="product-detail-container">
       <Header hasScrollEffect={false} />
-      <div style={{ height: 78 }} />
-      <h2>Detalles del Producto</h2>
-      <p>ID del Producto: {selectedProduct?.id}</p>
+
+      <BlockHeaderSpace />
+
+      <h2>Detalle del Producto</h2>
+
+      <div className="product-detail-wrapper">
+        <div className="product-detail">
+          <div className="product-detail-image">
+            <img src={image} alt={name} />
+          </div>
+
+          <ListItemText
+            itemList={infoProduct(selectedProduct!).itemList}
+            title={infoProduct(selectedProduct!).title}
+          />
+        </div>
+
+        <ListItemText
+          className="product-payment-method"
+          itemList={infoProductValues(selectedProduct!).itemList}
+          title={infoProductValues(selectedProduct!).title}
+        />
+      </div>
 
       <Footer />
     </div>
