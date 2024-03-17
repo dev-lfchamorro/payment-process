57ca57f2e6bd2ffdeb0d3511bd34908363fd4eb5
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-16 22:46:35 -0500

    Configuration to persistent data for global state wit Redux was added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,37 @@
-import { legacy_createStore as createStore } from "redux";
-import rootReducer from "./reducers";
+import {
+  Action,
+  Reducer,
+  Store,
+  UnknownAction,
+  legacy_createStore as createStore,
+} from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { rootReducer } from "./reducers";
 
-const store = createStore(rootReducer);
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+  }
+}
 
-export default store;
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  rootReducer as Reducer<unknown, Action>
+);
+
+const configureStore = () => {
+  return createStore(persistedReducer);
+};
+
+const store = configureStore();
+const persistor = persistStore(store as Store<any, UnknownAction, unknown>);
+
+export { persistor, store };
+
+export type AppState = ReturnType<typeof persistedReducer>;
