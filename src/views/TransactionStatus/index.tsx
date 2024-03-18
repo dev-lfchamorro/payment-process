c35ffe201ae7d0a52952c9e8ec30918a94702d2a
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-18 08:51:27 -0500

    Happy and unhappy paths were defined

--- src/views/TransactionStatus/index.tsx
+++ src/views/TransactionStatus/index.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 import Button from "../../components/Button";
 import CoverContent from "../../components/CoverContent/idex";
 import ListItemText from "../../components/ListItemText";
 import Footer from "../../components/common/Footer";
 import Header from "../../components/common/Header";
 import { paths } from "../../constants";
+import { RootState } from "../../redux/reducers";
+import { infoTransaction } from "../Products/constants";
 import IconLeft from "./../../assets/icons/arrow-left-circle.svg";
+import ImgFailed from "./../../assets/images/status-failed.png";
+import ImgPending from "./../../assets/images/status-pending.png";
+import ImgSuccess from "./../../assets/images/status-success.png";
 import ImgTransaction from "./../../assets/images/transaction.jpg";
 import "./styles.scss";
-import { infoTransaction } from "../Products/constants";
-import { useSelector } from "react-redux";
-import { RootState } from "../../redux/reducers";
-import { v4 as uuidv4 } from "uuid";
 
 const TransactionStatus: React.FC = () => {
   const navigate = useNavigate();
@@ -20,6 +23,11 @@ const TransactionStatus: React.FC = () => {
   const { paymentInfo, userInfo } = useSelector(
     (state: RootState) => state.payments
   );
+
+  const [iconStatus, setIconStatus] = useState(ImgPending);
+  const [textStatus, setTextStatus] = useState<
+    "pending" | "success" | "failed"
+  >("pending");
 
   const transactionStaus = infoTransaction({
     cardNumber: paymentInfo?.cardNumber || "",
@@ -29,8 +37,27 @@ const TransactionStatus: React.FC = () => {
     fullname: userInfo?.fullname || "",
     idTransaction: uuidv4(),
     phoneNumber: userInfo?.phoneNumber || "",
-    status: "pending",
+    status: textStatus,
   });
+
+  useEffect(() => {
+    if (
+      paymentInfo?.cardNumber === "5532123412344697" ||
+      paymentInfo?.cardNumber === "4532123412341234"
+    ) {
+      setIconStatus(ImgSuccess);
+      setTextStatus("success");
+    } else if (
+      paymentInfo?.cardNumber === "5532123412344369" ||
+      paymentInfo?.cardNumber === "4532123412347688"
+    ) {
+      setIconStatus(ImgFailed);
+      setTextStatus("failed");
+    } else {
+      setIconStatus(ImgPending);
+      setTextStatus("pending");
+    }
+  }, [paymentInfo?.cardNumber]);
 
   return (
     <div className="transaction-status-container">
@@ -42,13 +69,12 @@ const TransactionStatus: React.FC = () => {
 
       <div className="status-wrapper">
         <div className="icon-status">
-          <img src="" alt="" />
+          <img src={iconStatus} alt={textStatus} width={120} height={120} />
         </div>
-        <hr />
 
         <div className="pay-detail">
           <ListItemText
-            className="product-payment-method"
+            className="final-transaction-status"
             itemList={transactionStaus.itemList}
             title={transactionStaus.title}
           />
