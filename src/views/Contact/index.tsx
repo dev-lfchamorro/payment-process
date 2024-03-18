0ec44eec9d69049409e6958dd851d495010fe136
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-18 09:09:19 -0500

    Contact view component was updated

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Backdrop from "../../components/Backdrop";
 import CoverContent from "../../components/CoverContent/idex";
+import ListItemText from "../../components/ListItemText";
 import Footer from "../../components/common/Footer";
 import Header from "../../components/common/Header";
 import { RootState } from "../../redux/reducers";
 import ImgContact from "./../../assets/images/contact.avif";
+import ImgMe from "./../../assets/images/me.jpeg";
 import "./styles.scss";
-import Backdrop from "../../components/Backdrop";
 
 const Contact: React.FC = () => {
   const { showSummary } = useSelector((state: RootState) => state.payments);
@@ -19,79 +21,19 @@ const Contact: React.FC = () => {
         <div className="cover-content">Pregúntanos, estamos para ayudarte</div>
       </CoverContent>
 
-      <section>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          nulla impedit iusto quod consectetur, temporibus veniam maxime ipsa
-          numquam nostrum mollitia excepturi quam. Consectetur numquam tenetur,
-          ullam quibusdam blanditiis vel?
-        </p>
+      <section className="section-contact">
+        <img className="me-img" src={ImgMe} alt="me" />
+
+        <ListItemText
+          className="contact-card"
+          itemList={[
+            { title: "Teléfono", value: "+57 311 864 8813" },
+            { title: "Email", value: "ing.lfchamorro@gmail.com" },
+            { title: "Localización", value: "🇨🇴 Ibagué - Tolima" },
+            { title: "Rol", value: "Full Stack Developer" },
+          ]}
+          title="Info Contacto"
+        />
       </section>
 
       {showSummary && <Backdrop />}
