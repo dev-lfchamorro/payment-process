1e6b764271cf558185c4271916ea2b88d03a5533
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-17 19:45:47 -0500

    Checkbox component was created

---
+++ src/components/common/Checkbox/index.tsx
@@ -0,0 +1,33 @@
+import { ReactNode, useState } from "react";
+import "./styles.scss";
+
+export type CheckboxProps = {
+  id: string;
+  name: string;
+  children: ReactNode;
+};
+
+const Checkbox: React.FC<CheckboxProps> = ({ children, id, name }) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <div className="checkbox-wrapper">
+      <label>
+        <input
+          checked={checked}
+          id={id}
+          name={name}
+          onChange={handleChange}
+          type="checkbox"
+        />
+        {children}
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
