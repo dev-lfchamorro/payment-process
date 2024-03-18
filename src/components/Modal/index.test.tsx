6d7dc6401388d01c9b2df4a5ce8b2736697f70e8
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-18 12:14:43 -0500

    Testing Modal component

---
+++ src/components/Modal/index.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Modal from "./index"; // Asumiendo que Modal está en el mismo directorio
+
+describe("Modal component", () => {
+  test("renders when open", () => {
+    const onCloseMock = jest.fn();
+    render(
+      <Modal isOpen={true} onClose={onCloseMock}>
+        <div>Modal Content</div>
+      </Modal>
+    );
+
+    const modalOverlayElement = screen.getByTestId("modal-overlay");
+    expect(modalOverlayElement).toBeInTheDocument();
+
+    const modalContainerElement = screen.getByTestId("modal-container");
+    expect(modalContainerElement).toBeInTheDocument();
+
+    const modalContentElement = screen.getByText("Modal Content");
+    expect(modalContentElement).toBeInTheDocument();
+  });
+
+  test("closes when overlay is clicked", () => {
+    const onCloseMock = jest.fn();
+    render(
+      <Modal isOpen={true} onClose={onCloseMock}>
+        <div>Modal Content</div>
+      </Modal>
+    );
+
+    const modalOverlayElement = screen.getByTestId("modal-overlay");
+    fireEvent.click(modalOverlayElement);
+
+    expect(onCloseMock).toHaveBeenCalledTimes(1);
+  });
+});
